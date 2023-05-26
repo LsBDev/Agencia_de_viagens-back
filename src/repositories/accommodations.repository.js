0e2fb62c27ae6ab73557aec23b5a799c91edcc05
LsB.Dev <prof.luifel@gmail.com>
2023-05-26 10:58:49 -0300

    ajuste na query de hospedagens

--- src/repositories/accommodations.repository.js
+++ src/repositories/accommodations.repository.js
@@ -4,18 +4,18 @@ export function getAccommodationsCityIdDB(params) {
   const { id } = params
 
   const result = db.query(`
-    SELECT json_agg(json_build_object('photo_url', ph.photo_url)) AS photos, 
-      ac.name, 
-      ac.price, 
-      cv.pool, 
-      cv.air_conditioner,
-      cv.parking, 
-      cv.breakfast
-    FROM accommodation ac
-      JOIN photo ph ON ac.id = ph.accommodation_id
-      JOIN convenience cv ON ac.id = cv.accommodation_id
-      WHERE ac.city_id = $1
-    GROUP BY ac.id, ac.name, ac.price, cv.pool, cv.air_conditioner, cv.parking, cv.breakfast;
+    SELECT json_agg(json_build_object('photo_url', p.photo_url)) AS photos, 
+      a.name, 
+      a.price, 
+      c.pool, 
+      c.air_conditioner, 
+      c.parking, 
+      c.breakfast
+    FROM accommodation AS a
+      JOIN photo AS p ON a.id = p.accommodation_id
+      JOIN convenience AS c ON a.id = c.accommodation_id
+      WHERE a.city_id = $1
+    GROUP BY a.id, a.name, a.price, c.pool, c.air_conditioner, c.parking, c.breakfast;
   `, [id])
   return result
 }
