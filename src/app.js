61c72c9ac21f4eb1c7bd9697ef4cb71d416cf94d
LsB.Dev <prof.luifel@gmail.com>
2023-09-23 00:01:39 -0300

    fix: ajustes dotenv

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 import express, { json } from "express"
 import cors from "cors"
 import router from "./routes/index.routes.js"
+import dotenv from "dotenv"
+
+dotenv.config()
 
 const app = express()
 app.use(cors())
 app.use(json())
 app.use(router)
+
+app.get("/health", (req, res) => {
+  res.send("I'm Ok!")
+})
 
 
 const port = process.env.PORT || 5000
